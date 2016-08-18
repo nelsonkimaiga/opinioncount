ed0d7dc8eb0221b0cec971d59d0138339e285e54
kimaiga <nelsonkimaiga@aol.com>
2016-08-18 12:01:48 +0300

    Added db connector model

---
+++ opinion_poll_model.php
@@ -0,0 +1,63 @@
+<?php
+
+class Opinion_poll_model {
+
+ private $db_handle; private $host = 'localhost'; private $db = 'opinioncount';private $uid = 'root'; private $pwd = '303seminarian';
+
+    public function __construct() {
+
+        $this->db_handle = mysql_connect($this->host, $this->uid, $this->pwd); //connect to MySQL server
+
+        if (!$this->db_handle) die("Unable to connect to MySQL: " . mysql_error());
+
+        if (!mysql_select_db($this->db)) die("Unable to select database: " . mysql_error());
+
+    }
+
+    private function execute_query($sql_stmt) {
+
+        $result = mysql_query($sql_stmt); //execute SQL statement
+
+        return !$result ? FALSE : TRUE;
+
+    }
+
+    public function select($sql_stmt) {
+
+        $result = mysql_query($sql_stmt);
+
+        if (!$result) die("Database access failed: " . mysql_error());
+
+        $rows = mysql_num_rows($result);
+
+        $data = array();
+
+        if ($rows) {
+
+            while ($row = mysql_fetch_array($result)) {
+
+                $data = $row;
+
+            }
+
+        }
+
+        return $data;
+
+    }
+
+    public function insert($sql_stmt) {
+
+        return $this->execute_query($sql_stmt);
+
+    }
+
+    public function __destruct(){
+
+        mysql_close($this->db_handle);
+
+    }
+
+}
+
+?>
